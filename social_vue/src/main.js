05e1fc45c4886c61f141cc0a05d46dfbece29c70
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-07-30 19:26:54 +0500

    Create main.js

---
+++ social_vue/src/main.js
@@ -0,0 +1,19 @@
+import './assets/main.css'
+import './assets/min.scss'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router, axios)
+
+app.mount('#app')
