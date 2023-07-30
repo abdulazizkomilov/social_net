84559052318a9e9506991dabfefbbf6b3924f690
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-07-30 19:26:49 +0500

    Create index.js

---
+++ social_vue/src/router/index.js
@@ -0,0 +1,59 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import SignupView from '../views/SignupView.vue'
+import LoginView from '../views/LoginView.vue'
+import MessagesView from '../views/MessagesView.vue'
+import SearchView from '../views/SearchView.vue'
+import FormsView from '../views/FormsView.vue'
+import ProfileView from '../views/ProfileView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/create',
+      name: 'create',
+      component: FormsView
+    },
+    {
+      path: '/profile/:id',
+      name: 'profile',
+      component: ProfileView
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignupView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/messages',
+      name: 'messages',
+      component: MessagesView
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: SearchView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
+
+export default router
