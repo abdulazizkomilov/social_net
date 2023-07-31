1243d2c32b45370abb14fd778b785a4d5e6f1904
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-07-31 13:29:10 +0500

    Create App.vue

---
+++ social_vue/src/App.vue
@@ -0,0 +1,160 @@
+<template>
+    <nav class="bg-white border-gray-200 dark:bg-gray-900">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
+
+            <RouterLink :to="{ name: 'home' }" class="flex items-center">
+                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="" />
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+            </RouterLink>
+            <div class="flex items-center md:order-2">
+                <template v-if="userStore.user.isAuthenticated && userStore.user.id">
+                    <button type="button"
+                        class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                        id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown"
+                        data-dropdown-placement="bottom">
+                        <span class="sr-only">Open user menu</span>
+                        <img class="w-8 h-8 rounded-full" src="https://github.com/abdulazizkomilov.png" alt="user photo">
+                    </button>
+                </template>
+                <template v-else>
+                    <RouterLink :to="{ name: 'login' }"
+                        class="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-2 py-2 text-center mr-2 mb-2">
+                        Login</RouterLink>
+                </template>
+                <!-- Dropdown menu -->
+                <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+                    id="user-dropdown">
+                    <div class="px-4 py-3">
+                        <span class="block text-sm text-gray-900 dark:text-white">{{ userStore.user.name }}</span>
+                        <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">{{ userStore.user.email
+                        }}</span>
+                    </div>
+                    <ul class="py-2" aria-labelledby="user-menu-button">
+                        <li>
+                            <RouterLink :to="{ name: 'profile', params: { 'id': userStore.user.id } }"
+                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                                Profile</RouterLink>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
+                        </li>
+                        <li>
+                            <RouterLink :to="{ name: 'create' }"
+                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                                Add
+                                post</RouterLink>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                                Sign
+                                out</a>
+                        </li>
+                    </ul>
+                </div>
+                <button data-collapse-toggle="navbar-user" type="button"
+                    class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                    aria-controls="navbar-user" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 17 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M1 1h15M1 7h15M1 13h15" />
+                    </svg>
+                </button>
+            </div>
+            <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
+                <ul
+                    class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <li>
+                        <RouterLink :to="{ name: 'home' }"
+                            class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                            aria-current="page">Blogs</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink :to="{ name: 'messages' }"
+                            class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                            aria-current="page">Messages</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink :to="{ name: 'home' }"
+                            class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                            Chat</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink :to="{ name: 'home' }"
+                            class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                            Notifications</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink :to="{ name: 'search' }"
+                            class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                            Search</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink to="/about"
+                            class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                            About</RouterLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+    <main>
+        <div class="bg-gray-100 mx-auto max-w-7xl py-4 sm:px-6 lg:px-6">
+            <RouterView />
+        </div>
+        <div class="rounded-full" id="mybutton" style="position: fixed; bottom: 20px; right: 8px;">
+            <button class="feedback rounded-full"
+                style="background-color : #31B0D5; color: white; padding: 10px 10px; border-color: #46b8da;">Chat</button>
+        </div>
+    </main>
+
+    <Toast />
+
+    <footer class="mx-auto w-full max-w-container px-4 sm:px-6 lg:px-6">
+        <div class="border-t border-slate-900/5 py-4">
+            <p class="mt-5 text-center text-sm leading-6 text-slate-500">© 2023 Tailwind Labs Inc. All rights reserved.</p>
+            <div class="mt-6 flex items-center justify-center space-x-4 text-sm font-semibold leading-6 text-slate-700"><a
+                    href="/privacy-policy">Privacy policy</a>
+                <div class="h-4 w-px bg-slate-500/20"></div><a href="/changelog">Changelog</a>
+            </div>
+        </div>
+    </footer>
+</template>
+
+
+<script>
+import axios from 'axios'
+import Toast from '@/components/Toast.vue'
+import { useUserStore } from '@/stores/user'
+
+export default {
+    setup() {
+        const userStore = useUserStore()
+
+        return {
+            userStore
+        }
+    },
+
+    components: {
+        Toast
+    },
+
+    beforeCreate() {
+        this.userStore.initStore()
+
+        const token = this.userStore.user.access
+
+        if (token) {
+            axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+        } else {
+            axios.defaults.headers.common["Authorization"] = "";
+        }
+    }
+}
+</script>
