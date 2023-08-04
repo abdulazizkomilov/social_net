8006ee1117b181c2ad60458f9fcc5e6a9535c70c
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-08-04 14:24:18 +0500

    Update App.vue

--- social_vue/src/App.vue
+++ social_vue/src/App.vue
@@ -31,7 +31,7 @@
                     </div>
                     <ul class="py-2" aria-labelledby="user-menu-button">
                         <li>
-                            <RouterLink :to="{ name: 'profile', params: { 'id': userStore.user.id } }"
+                            <RouterLink :to="{ name: 'create' }"
                                 class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
                                 Profile</RouterLink>
                         </li>
